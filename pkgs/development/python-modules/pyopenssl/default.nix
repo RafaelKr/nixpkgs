219daa344160eff519846f2ba629b1fbedf99d37
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-04-18 21:48:33 +0200

    python3Packages.pyopenssl: 24.0.0 -> 24.1.0
    https://github.com/pyca/pyopenssl/blob/24.1.0/CHANGELOG.rst

--- pkgs/development/python-modules/pyopenssl/default.nix
+++ pkgs/development/python-modules/pyopenssl/default.nix
@@ -14,13 +14,13 @@
 
 buildPythonPackage rec {
   pname = "pyopenssl";
-  version = "24.0.0";
+  version = "24.1.0";
   pyproject = true;
 
   src = fetchPypi {
     pname = "pyOpenSSL";
     inherit version;
-    hash = "sha256-aqMwOak//6RWPmVbYdETZNASZL6My0mQYQHgKjNFML8=";
+    hash = "sha256-yr7Uv6pd+fGhbA72Sgy2Uxi1zQd6ftp9aXATHKL0Gm8=";
   };
 
   outputs = [
