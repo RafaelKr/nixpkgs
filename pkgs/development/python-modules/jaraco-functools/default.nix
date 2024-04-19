dcb6b44ca74652d020234ae3166b967d1f791f4c
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-04-19 10:03:27 +0200

    fixup! python3Packages.jaraco-functools: 4.0.0 -> 4.0.1

--- pkgs/development/python-modules/jaraco-functools/default.nix
+++ pkgs/development/python-modules/jaraco-functools/default.nix
@@ -2,21 +2,25 @@
 , buildPythonPackage
 , fetchPypi
 , more-itertools
+, setuptools
 , setuptools-scm
 }:
 
 buildPythonPackage rec {
   pname = "jaraco-functools";
   version = "4.0.1";
-  format = "pyproject";
+  pyproject = true;
 
   src = fetchPypi {
-    pname = "jaraco.functools";
+    pname = "jaraco_functools";
     inherit version;
     hash = "sha256-0z+nZTdMBhG1L4s6eV+JAIaaqIyEdp1NF0bNaPsow+g=";
   };
 
-  nativeBuildInputs = [ setuptools-scm ];
+  nativeBuildInputs = [
+    setuptools
+    setuptools-scm
+  ];
 
   propagatedBuildInputs = [
     more-itertools
